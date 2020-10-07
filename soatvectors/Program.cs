d02fd6984e8e1c41d94b024391182651cd1832b1
Mattia Filippi <MATTIA.FILIPPI@local.ittcesena.it>
2020-10-07 11:07:37 +0200

    first step

---
+++ soatvectors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace soatvectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
